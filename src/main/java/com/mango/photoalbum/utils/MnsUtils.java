8b6a1c75c1dfa8f40928b6b9d5e344a33d850f71
swen <1063642821@qq.com>
2019-12-19 16:09:41 +0800

    添加图片识别与mns

--- src/main/java/com/mango/photoalbum/utils/MnsUtils.java
+++ src/main/java/com/mango/photoalbum/utils/MnsUtils.java
@@ -4,9 +4,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.aliyun.mns.client.CloudAccount;
 import com.aliyun.mns.client.CloudQueue;
+import com.aliyun.mns.client.CloudTopic;
 import com.aliyun.mns.client.MNSClient;
-import com.aliyun.mns.model.Message;
-import com.aliyun.mns.model.QueueMeta;
+import com.aliyun.mns.common.http.ClientConfiguration;
+import com.aliyun.mns.model.*;
+import com.aliyun.mns.sample.HttpEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -29,12 +31,21 @@ public class MnsUtils {
     @Value("${alibaba.mns.endpoint}")
     private String endpoint;
 
+    @Value("${alibaba.mns.threadNum:100}")
+    private int threadNum;
+
+    @Value("${alibaba.mns.totalSeconds:180}")
+    private int totalSeconds;
+
     private MNSClient mns;
 
     @PostConstruct
     private void init() {
         try {
-            CloudAccount account = new CloudAccount(accessKeyId, accessKeySecret, endpoint);
+            ClientConfiguration clientConfiguration = new ClientConfiguration();
+            clientConfiguration.setMaxConnections(threadNum);
+            clientConfiguration.setMaxConnectionsPerRoute(threadNum);
+            CloudAccount account = new CloudAccount(accessKeyId, accessKeySecret, endpoint, clientConfiguration);
             mns = account.getMNSClient();
         } catch (Exception e){
             e.printStackTrace();
@@ -59,6 +70,98 @@ public class MnsUtils {
         } catch (Exception e) {
             e.printStackTrace();
             log.info("MNS创建队列出现错误:{}", e.getMessage());
+        }
+    }
+
+    /**
+     * 创建topic
+     * @param topicName
+     */
+    public void createTopic(String topicName) {
+        try {
+            log.info("MNS创建topic开始:{}", topicName);
+            TopicMeta meta = new TopicMeta();
+            meta.setTopicName(topicName);
+            CloudTopic topic = mns.createTopic(meta);
+            log.info("MNS创建topic成功：{}", JSONObject.toJSONString(topic, SerializerFeature.IgnoreNonFieldGetter));
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("MNS创建topic出现错误:{}", e.getMessage());
+        }
+    }
+
+    /**
+     * 删除topic
+     * @param topicName
+     */
+    public void deleteTopic(String topicName) {
+        try {
+            log.info("MNS删除topic开始:{}", topicName);
+            CloudTopic topic = mns.getTopicRef(topicName);
+            topic.delete();
+            log.info("MNS删除topic成功：{}", JSONObject.toJSONString(topic, SerializerFeature.IgnoreNonFieldGetter));
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("MNS删除topic出现错误:{}", e.getMessage());
+        }
+    }
+
+    /**
+     * 创建订阅
+     * @param topicName
+     * @param subscriptionName
+     */
+    public void subscribe(String topicName, String subscriptionName) {
+        try {
+            log.info("MNS创建订阅开始:{},{}", topicName, subscriptionName);
+            CloudTopic topic = mns.getTopicRef(topicName);
+            SubscriptionMeta subMeta = new SubscriptionMeta();
+            subMeta.setSubscriptionName(subscriptionName);
+            subMeta.setEndpoint(HttpEndpoint.GenEndpointLocal());
+            subMeta.setNotifyContentFormat(SubscriptionMeta.NotifyContentFormat.XML);
+            //subMeta.setFilterTag("filterTag"); //设置订阅的filterTag
+            String subUrl = topic.subscribe(subMeta);
+            log.info("MNS创建订阅成功:{}", subUrl);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("MNS创建订阅出现错误:{}", e.getMessage());
+        }
+    }
+
+    /**
+     * 发布消息
+     * @param topicName
+     * @param messageBody
+     */
+    public void publishMessage(String topicName, String messageBody) {
+        try {
+            log.info("MNS发布消息开始:{},{}", topicName, messageBody);
+            CloudTopic topic = mns.getTopicRef(topicName);
+            TopicMessage msg = new Base64TopicMessage(); //可以使用TopicMessage结构，选择不进行Base64加密
+            msg.setMessageBody(messageBody);
+            //msg.setMessageTag("filterTag"); //设置该条发布消息的filterTag
+            msg = topic.publishMessage(msg);
+            log.info("MNS创建订阅成功:{}", JSONObject.toJSONString(msg, SerializerFeature.IgnoreNonFieldGetter));
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("MNS创建订阅出现错误:{}", e.getMessage());
+        }
+    }
+
+    /**
+     * 取消订阅
+     * @param topicName
+     * @param subscriptionName
+     */
+    public void unSubscribe(String topicName, String subscriptionName) {
+        try {
+            log.info("MNS取消订阅开始:{},{}", topicName, subscriptionName);
+            CloudTopic topic = mns.getTopicRef(topicName);
+            topic.unsubscribe(subscriptionName);
+            log.info("MNS取消订阅成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("MNS取消订阅出现错误:{}", e.getMessage());
         }
     }
 
