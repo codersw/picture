1228ccc818cd87cc5c3be98ccea0ff0d85cb8260
swen <1063642821@qq.com>
2019-12-25 17:54:47 +0800

    集成RocketMQ配置

--- src/main/java/com/mango/photoalbum/constant/PermissionConst.java
+++ src/main/java/com/mango/photoalbum/constant/PermissionConst.java
@@ -2,7 +2,13 @@ package com.mango.photoalbum.constant;
 
 public class PermissionConst {
 
+    /**
+     * 管理员
+     */
     public static final String SUPPERUSERFLAGENUM = "1";
 
+    /**
+     * 普通用户
+     */
     public static final String NOSUPPERUSERFLAGENUM = "0";
 }
