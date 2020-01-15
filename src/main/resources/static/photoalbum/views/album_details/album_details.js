390f67780d2fb78bc17bcb0517045d518acb4983
swen <1063642821@qq.com>
2020-01-15 16:46:15 +0800

    添加是否公开字段

--- src/main/resources/static/photoalbum/views/album_details/album_details.js
+++ src/main/resources/static/photoalbum/views/album_details/album_details.js
@@ -19,7 +19,7 @@ $(function () {
         }
         var albumId = location.href.substring(location.href.indexOf('=') + 1, titleAnd);
         $.ajax({
-            url: serverHost + '/upload/v2/list?albumId=' + albumId + '&orgId=' + getOrigId().origId,
+            url: serverHost + '/upload/v2/list?albumId=' + albumId + '&orgId=' + getOrigId().origId + '&orgIdAll=' + getOrigId().orgIdAll,
             type: 'GET',
             headers: { 'userId': id },
             success: function (res) {
