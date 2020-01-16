afc251575385a7db134c6a15532f386059da7bcd
swen <1063642821@qq.com>
2020-01-16 14:08:33 +0800

    添加是否公开字段

--- src/main/java/com/mango/photoalbum/service/impl/FaceConsumer.java
+++ src/main/java/com/mango/photoalbum/service/impl/FaceConsumer.java
@@ -2,11 +2,14 @@ package com.mango.photoalbum.service.impl;
 
 import com.mango.photoalbum.model.UploadFile;
 import com.mango.photoalbum.service.FaceService;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * topic消费
@@ -17,15 +20,18 @@ import javax.annotation.Resource;
 @Slf4j
 @Service
 @RocketMQMessageListener(topic = "${alibaba.mq.topic}", consumerGroup = "${alibaba.mq.group}",
-        accessKey = "${alibaba.mq.accessKeyId}", secretKey = "${alibaba.mq.accessKeySecret}")
+        accessKey = "${alibaba.mq.accessKeyId}", secretKey = "${alibaba.mq.accessKeySecret}",
+        consumeMode = ConsumeMode.ORDERLY, consumeThreadMax = 1)
 public class FaceConsumer implements RocketMQListener<UploadFile> {
 
     @Resource
     private FaceService faceService;
 
+    @SneakyThrows
     @Override
     public void onMessage(UploadFile uploadFile) {
         log.info("监听到信息:{}", uploadFile.toString());
         faceService.handleFace(uploadFile);
+        TimeUnit.SECONDS.sleep(1);
     }
 }
